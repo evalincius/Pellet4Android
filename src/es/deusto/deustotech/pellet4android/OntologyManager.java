a98c741f6b69ef093fa6444115a86957ef736679
evalincius <ed@edgarass-mbp.home>
2015-03-04 16:33:54 +0000

    method moved from OntologyManager class
    moved to the AdapUIDemo project

--- src/es/deusto/deustotech/pellet4android/OntologyManager.java
+++ src/es/deusto/deustotech/pellet4android/OntologyManager.java
@@ -1185,40 +1185,6 @@ public class OntologyManager {
 	
 	
 	
-	public int executeQueries(String ontology, String[] queries) {
-		
-		for ( int i = 0; i < queries.length ; i++ ) {
-			String query = queries[i];
-			
-			// First create a Jena ontology model backed by the Pellet reasoner
-			// (note, the Pellet reasoner is required)
-			OntModel m = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC);
 	
-			// Then read the data from the file into the ontology model
-			m.read( ontology );
-	
-			// Now read the query file into a query object
-			Query q = QueryFactory.read( query );
-	
-			// Create a SPARQL-DL query execution for the given query and
-			// ontology model
-			QueryExecution qe = SparqlDLExecutionFactory.create( q, m );
-	
-			// We want to execute a SELECT query, do it, and return the result set
-			ResultSet rs = qe.execSelect();
-	
-			// Print the query for better understanding
-			System.out.println(q.toString());
-			
-			// There are different things we can do with the result set, for
-			// instance iterate over it and process the query solutions or, what we
-			// do here, just print out the results
-			ResultSetFormatter.out( rs );
-			
-			// And an empty line to make it pretty
-			System.out.println();
-		}
-		return 1;
-	}
 
 }
